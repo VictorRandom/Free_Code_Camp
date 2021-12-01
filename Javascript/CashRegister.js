4f9a326c2a936b05115183f4fcf44176e509a303
Victorrandom <victormbalduino@hotmail.com>
2021-12-01 18:29:06 -0300

    Javascript Project - Cash Register

---
+++ Javascript/CashRegister.js
@@ -0,0 +1,9 @@
+function checkCashRegister(price, cash, cid) {
+
+    let change;
+    change = price - cash
+    
+    return change;
+  }
+  
+  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
